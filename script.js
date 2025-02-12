439e59532ee726281304581d03f558f9be455c0e
yuanwei0116 <e27977237@gmail.com>
2025-02-12 14:44:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let count = 0;
+const feedButton = document.getElementById("feedButton");
+const foodCounter = document.getElementById("foodCounter");
+const doll = document.getElementById("doll");
+const feedingImages = ["feed.jpg", "bruh.jpg","ghost.jpg"];
+feedButton.addEventListener("click", function () {
+    count++;
+    foodCounter.innerText = "餵食次數：" + count;
+    let randomFeedingImage = feedingImages[Math.floor(Math.random() * feedingImages.length)];
+    doll.src = randomFeedingImage; 
+    if (randomFeedingImage=="bruh.jpg"||randomFeedingImage=="ghost.jpg") {
+        count=0;
+        foodCounter.innerText = "餵食次數：" + count;
+    }
+    if (randomFeedingImage === "ghost.jpg") {
+        alert("被阿框搶劫!");
+        setTimeout(() => {
+            doll.src = randomFeedingImage;
+        }, 1000);
+    }
+    setTimeout(() => {
+        doll.src = "ppc.jpg";
+    }, 1000);
+});
